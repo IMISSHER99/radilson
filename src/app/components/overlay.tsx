614d5d12cbc9008797e6285584040adfd5fd3285
mrrobot <mrrobot@kali>
2024-03-16 21:52:34 +0530

    ADDED HEADER AND COMPLETED LOGO

---
+++ src/app/components/overlay.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+interface OverlayProps {
+
+}
+
+const Overlay: React.FC<OverlayProps> = () => {
+
+    const [translateX, setTranslateX] = useState(9);
+    const [translateY, setTranslateY] = useState(9);
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        const newTranslateX = translateX > -15 ? translateX - 1 : 9;
+        const newTranslateY = translateY > -15 ? translateY - 1 : 9;
+  
+        setTranslateX(newTranslateX);
+        setTranslateY(newTranslateY);
+      }, 100); 
+  
+      return () => clearInterval(intervalId);
+    }, [translateX, translateY]);
+
+    return (
+        <div className='relative w-full h-full overflow-hidden'>
+            <div style={{
+            background: 'url(https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png)',
+            opacity: '.03',
+            inset: '-200%',
+            width: '400%',
+            height: '400%',
+            position: 'absolute',
+            transform: `translate(${translateX}%, ${translateY}%)`
+            }}>
+        </div>
+    </div>
+    )
+}
+
+export default Overlay;
---
+++ src/app/components/sunburst.tsx
@@ -0,0 +1,14 @@
+interface SunburstProps {
+
+}
+
+const Sunburst: React.FC<SunburstProps> = () => {
+
+    return (
+        <div className="flex items-center justify-center flex-nowrap">
+            
+        </div>
+    )
+}
+
+export default Sunburst;
